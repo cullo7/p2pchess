1c4d6228410679ad8dabcdc464a9aaf9a6f0fb66
Aiden Cullo <aidencullo@gmail.com>
2022-12-19 15:49:28 -0500

    successfully opens connection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-// caller side
+// peer declarations
 const caller = new Peer('initiatorId', { debug: 3 });
 const callee = new Peer('calleId', { debug: 3 });
 
-var conn = caller.connect('calleId');
+//caller listeners
+caller.on("open", () => {
+    alert("caller opened");
+});
 
-caller.on("open", () => {alert("caller opened");});
-callee.on("open", () => {alert("callee opened");});
-printname = (str) => {alert(str);};
 
-caller.on("connection", printname("connection"));
+// callee listeners
+callee.on("open", () => {
+    alert("callee opened");
+    // connection needs to be declared here so that all the listeners are initialized
+    const conn = caller.connect('calleId');
+});
 
+callee.on("connection",  () => {
+    alert("connection received by callee");
+});
