5a7c1702200011bdb3deff7233e97c2373632705
Aiden Cullo <aidencullo@gmail.com>
2022-12-19 16:39:50 -0500

    sent data over p2p channel

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 // peer declarations
 const caller = new Peer('initiatorId', { debug: 3 });
 const callee = new Peer('calleId', { debug: 3 });
+var conn;
 
 //caller listeners
 caller.on("open", () => {
-    alert("caller opened");
+//    alert("caller opened");
 });
-
 
 // callee listeners
 callee.on("open", () => {
     alert("callee opened");
     // connection needs to be declared here so that all the listeners are initialized
-    const conn = caller.connect('calleId');
+    conn = caller.connect('calleId');
 });
 
-callee.on("connection",  () => {
-    alert("connection received by callee");
+callee.on("connection", (otroconn) => {
+    alert('peer connected');
+    otroconn.on('open', function() {
+        console.log('conn open');
+	sendMessage("hi");
+
+    });
+    otroconn.on('data', function(data) {
+        console.log(data);
+    });
 });
+
+function sendMessage(s) {
+    conn.send(s);
+}
