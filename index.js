17e372e6ddb2d1f17c123a05408521f7a6c5d8a8
Aiden Cullo <aidencullo@gmail.com>
2022-12-20 14:52:49 -0500

    chat function working

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
 // peer declarations
 const local = new Peer();
-var conn = undefined;
+var incomingConnection = undefined;
+var outgoingConnection = undefined;
 
 local.on("open", (id) => {
-    //    alert("my id is " + id);
     document.getElementById("peerid").value = id;
 });
 
 local.on("connection", (otroconn) => {
     otroconn.on('open', function() {
-	alert("connection opened");
+	if (outgoingConnection == undefined) {
+	    outgoingConnection = local.connect(otroconn.peer);
+	}
     });
     otroconn.on('data', function(data) {
         postMessageReceived(data);
     });
-    conn = otroconn;
+    incomingConnection = otroconn;
 });
 
 function sendMessage(s) {
-    if (conn == undefined) {
-	alert("connection not established");
+    if (outgoingConnection == undefined) {
     } else {
-	conn.send(s);
+	outgoingConnection.send(s);
 	postMessageSent(s);
-	
     }
 }
 
 var input = document.getElementById("message");
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-	sendMessage(input.value);
-	input.value = "";
+	if (input.value != "") {
+	    sendMessage(input.value);
+	    input.value = "";
+	}
     }
 });
 
 var otroIp = document.getElementById("otropeerid");
-var connectButton = document.getElementById("connect");
-connectButton.onclick = () => {
-    if (otroIp.value == "") {
-	alert("missing remote IP");
-    } else {
-	alert("trying to connect");
-	conn = local.connect(otroIp.value);
+otroIp.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+	if (otroIp.value != "") {
+	    outgoingConnection = local.connect(otroIp.value);
+	} else {
+	    alert("no IP provided");
+	}
     }
-}
+});
 
 // Jquery for chat functionality
 
 $(document).ready(function(){
     postMessageReceived = function(message) {
 	$("#chat").append(
-	    "<div class='receiver'>" + message + " </div>"
+	    "<div class='receiver message'>" + message + " </div>"
 	);
     }
 
     postMessageSent = function(message) {
 	$("#chat").append(
-	    "<div class='sender'>" + message + " </div>"
+	    "<div class='sender message'>" + message + " </div>"
 	);
     }
 
 });
+
+window.setInterval(function() {
+    var elem = document.getElementById('chat');
+    elem.scrollTop = elem.scrollHeight;
+}, 500);
