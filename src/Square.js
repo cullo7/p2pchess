4963bd14a4c060ff9f8eb38b57116880e3752e68
Aiden Cullo <aidencullo@gmail.com>
2023-01-04 20:59:41 -0500

    board setup with piece images

--- src/Square.js
+++ src/Square.js
@@ -1,16 +1,84 @@
 import React from 'react';
 
-import Knight from './Knight.js';
+// react components
+import Piece from './Piece.js';
+
+// Global vars
+var BOARD_WIDTH = 8;
 
 export default class Square extends React.Component {
+
+    constructor(props) {
+	super(props);
+	this.index = this.props.id;
+	this.row = Math.floor(this.index / BOARD_WIDTH);
+	this.rowIndex = this.index % BOARD_WIDTH;
+    }
+    
     getColor() {
-	var index = this.props.id;
-	var indexEight = Math.floor(index/8);
-	if (indexEight % 2 == 0) {
-	    return index % 2 == 0 ? "red" : "white";
+	if (this.row % 2 == 0) {
+	    return this.index % 2 == 0 ? "red" : "white";
 	} else {
-	    return index % 2 == 0 ? "white" : "red";
+	    return this.index % 2 == 0 ? "white" : "red";
 	}
+    }
+    
+    getPiece() {
+	let side;
+	if (this.row == 0 || this.row == 1) {
+	    side = 0;
+	} else {
+	    side = 1;
+	}
+	switch (this.row) {
+	case 0:
+	case 7:
+	    return this.getMainPiece(side)
+	    break;
+	case 1:
+	case 6:
+	    return this.getPawn(side)
+	    break;
+	default:
+	    return;
+	}
+	return this.row;
+    }
+
+    getMainPiece(c) {
+	switch(this.rowIndex) {
+	case 0:
+	    return <Piece type="r" color={c}/>;
+	    break;
+	case 1:
+	    return <Piece type="kn" color={c}/>;
+	    break;
+	case 2:
+	    return <Piece type="b" color={c}/>;
+	    break;
+	case 3:
+	    return <Piece type="q" color={c}/>;
+	    break;
+	case 4:
+	    return <Piece type="k" color={c}/>;
+	    break;
+	case 5:
+	    return <Piece type="b" color={c}/>;
+	    break;
+	case 6:
+	    return <Piece type="kn" color={c}/>;
+	    break;
+	case 7:
+	    return <Piece type="r" color={c}/>;
+	    break;
+	default:
+	    console.log("default hit");
+	    return;
+	}
+    }
+
+    getPawn(c) {
+	return <Piece type="p" color={c}/>;
     }
     
     render() {
@@ -19,7 +87,7 @@ export default class Square extends React.Component {
 		    onClick={() => this.props.onClick()}
 		    style={{backgroundColor: this.getColor()}}
 	    >
-		<Knight/>
+		{this.getPiece()}
 	    </button>
 	);
     }
