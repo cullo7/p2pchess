63a3eebfd70ce9d235d3693ba975f0cbf65a5f08
Aiden Cullo <aidencullo@gmail.com>
2023-01-06 19:00:48 -0500

    moving logic and board state into game component (I think i will just make board and game one component and delete the piece component

--- src/Game.js
+++ src/Game.js
@@ -2,18 +2,91 @@ import React from 'react';
 
 import Square from './Square.js';
 
+// Global vars
+var BOARD_WIDTH = 8;
+
 export default class Game extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.state = {squares: new Array(64).fill(null)};
+	this.state = {squares: new Array(64).fill({
+	    color: undefined,
+	    piece: undefined,
+	})};
+	this.initializeBoard();
     }
 
+    initializeBoard() {
+	for (let index = 0; index < this.state.squares.length; index++) {
+	    let row = Math.floor(index / BOARD_WIDTH);
+	    let rowIndex = index % BOARD_WIDTH;
+	    let side;
+	    if (row == 0 || row == 1) {
+		side = 0;
+	    } else {
+		side = 1;
+	    }
+	    switch (row) {
+	    case 0:
+	    case 7:
+		return this.setMainPiece(side, rowIndex);   //// basically got here
+		break;
+	    case 1:
+	    case 6:
+		return this.getPawn(side, rowIndex);
+		break;
+	    default:
+		return;
+	    }
+	    return row;
+	}
+    }
+    
+    getMainPiece(c, rowIndex) {
+	switch(rowIndex) {
+	case 0:
+	    return <Piece type="r" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 1:
+	    return <Piece type="kn" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 2:
+	    return <Piece type="b" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 3:
+	    return <Piece type="q" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 4:
+	    return <Piece type="k" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 5:
+	    return <Piece type="b" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 6:
+	    return <Piece type="kn" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	case 7:
+	    return <Piece type="r" color={c} onClick={() => this.props.move()}/>;
+	    break;
+	default:
+	    console.log("default hit");
+	    return;
+	}
+    }
+
+    getPawn(c) {
+	return <Piece type="p" color={c} onClick={() => this.props.move()}/>;
+    }
+
+    handleClick(index) {
+	console.log("handleClick in Game:");
+    }
+    
     render() {
 	return (
 	    <div>
 	    	{this.state.squares.map((square,index) => (
-		    <Square key={index} id={index} onClick={this.props.click}/>
+		    <Square key={index} id={index} move={() => handleClick(index)} piece={() => getPiece()}/>
 		))}
 
 	    </div>
--- src/Square.js
+++ src/Square.js
@@ -3,16 +3,10 @@ import React from 'react';
 // react components
 import Piece from './Piece.js';
 
-// Global vars
-var BOARD_WIDTH = 8;
-
 export default class Square extends React.Component {
 
     constructor(props) {
 	super(props);
-	this.index = this.props.id;
-	this.row = Math.floor(this.index / BOARD_WIDTH);
-	this.rowIndex = this.index % BOARD_WIDTH;
     }
     
     getColor() {
@@ -23,72 +17,14 @@ export default class Square extends React.Component {
 	}
     }
     
-    getPiece() {
-	let side;
-	if (this.row == 0 || this.row == 1) {
-	    side = 0;
-	} else {
-	    side = 1;
-	}
-	switch (this.row) {
-	case 0:
-	case 7:
-	    return this.getMainPiece(side)
-	    break;
-	case 1:
-	case 6:
-	    return this.getPawn(side)
-	    break;
-	default:
-	    return;
-	}
-	return this.row;
-    }
-
-    getMainPiece(c) {
-	switch(this.rowIndex) {
-	case 0:
-	    return <Piece type="r" color={c}/>;
-	    break;
-	case 1:
-	    return <Piece type="kn" color={c}/>;
-	    break;
-	case 2:
-	    return <Piece type="b" color={c}/>;
-	    break;
-	case 3:
-	    return <Piece type="q" color={c}/>;
-	    break;
-	case 4:
-	    return <Piece type="k" color={c}/>;
-	    break;
-	case 5:
-	    return <Piece type="b" color={c}/>;
-	    break;
-	case 6:
-	    return <Piece type="kn" color={c}/>;
-	    break;
-	case 7:
-	    return <Piece type="r" color={c}/>;
-	    break;
-	default:
-	    console.log("default hit");
-	    return;
-	}
-    }
-
-    getPawn(c) {
-	return <Piece type="p" color={c}/>;
-    }
-    
     render() {
 	return (
-	    <button className="square"
-		    onClick={() => this.props.onClick()}
-		    style={{backgroundColor: this.getColor()}}
+	    <div
+		className="square"
+		style={{backgroundColor: this.getColor()}}
 	    >
-		{this.getPiece()}
-	    </button>
+		{this.props.atSquare()}
+	    </div>
 	);
     }
 }
